64d93cee19fca09e3e85cf33a762735ea501ffdb
Thiago Prazeres <thiagoprazeres@gmail.com>
2025-04-22 16:23:29 -0300

    SEO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,23 +7,28 @@ export const routes: Routes = [
     },
     {
         path: 'sobre',
-        loadComponent: () => import('./pages/sobre/sobre.component').then(m => m.SobreComponent)
+        loadComponent: () => import('./pages/sobre/sobre.component').then(m => m.SobreComponent),
+        title: 'Sobre | Dot7 Tecnologia'
     },
     {
         path: 'solucoes',
-        loadComponent: () => import('./pages/solucoes/solucoes.component').then(m => m.SolucoesComponent)
+        loadComponent: () => import('./pages/solucoes/solucoes.component').then(m => m.SolucoesComponent),
+        title: 'Soluções | Dot7 Tecnologia'
     },
     {
         path: 'solucoes/cp7-school',
-        loadComponent: () => import('./pages/solucoes/cp7-school/cp7-school.component').then(m => m.Cp7SchoolComponent)
+        loadComponent: () => import('./pages/solucoes/cp7-school/cp7-school.component').then(m => m.Cp7SchoolComponent),
+        title: 'CP7 School | Dot7 Tecnologia'
     },
     {
         path: 'solucoes/vou-buscar',
-        loadComponent: () => import('./pages/solucoes/vou-buscar/vou-buscar.component').then(m => m.VouBuscarComponent)
+        loadComponent: () => import('./pages/solucoes/vou-buscar/vou-buscar.component').then(m => m.VouBuscarComponent),
+        title: 'VouBusCar | Dot7 Tecnologia'
     },
     {
         path: 'solucoes/equipamentos',
-        loadComponent: () => import('./pages/solucoes/equipamentos/equipamentos.component').then(m => m.EquipamentosComponent)
+        loadComponent: () => import('./pages/solucoes/equipamentos/equipamentos.component').then(m => m.EquipamentosComponent),
+        title: 'Equipamentos | Dot7 Tecnologia'
     },
     // {
     //     path: 'solucoes/fabrica',
@@ -31,6 +36,7 @@ export const routes: Routes = [
     // },
     {
         path: 'contato',
-        loadComponent: () => import('./pages/contato/contato.component').then(m => m.ContatoComponent)
+        loadComponent: () => import('./pages/contato/contato.component').then(m => m.ContatoComponent),
+        title: 'Contato | Dot7 Tecnologia'
     }
 ];
